46579bf301c91fdb72ab8f7ba3c9f8be3549c36d
Ryan Ramos <ryan.ramosenhs@gmail.com>
2022-02-28 12:28:16 -0600

    calendar api

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from __future__ import print_function
+
+import datetime
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 import os
 import time
 import gtts
@@ -5,10 +15,91 @@ import playsound
 import speech_recognition as sr
 from gtts import gTTS
 
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
 def speak(text):
     tts = gTTS(text=text, lang="en")
     filename = "voice.mp3"
     tts.save(filename)
     playsound.playsound(filename)
 
-speak("hello")
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source: # could also use a text file
+        audio = r.listen(source)
+        said = ""
+
+        try:
+            said = r.recognize_google(audio)
+            print("Interpreted message: " + said)
+        except Exception as e:
+            print("Exception: " + str(e))
+
+    return said
+
+# speak("hello")
+# get_audio()
+
+# text = get_audio()
+
+# if "hello" in text:
+#     speak("hello, how are you?")
+
+# if "what is your name" in text:
+#     speak("my name is claire")
+
+
+
+
+def authenticate_google():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('calendar', 'v3', credentials=creds)
+
+        return service
+
+    except HttpError as error:
+        print('An error occurred: %s' % error)
+
+def get_events(n, service):
+    # Call the Calendar API
+        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        print(f'Getting the upcoming {n} events')
+        events_result = service.events().list(calendarId='primary', timeMin=now,
+                                              maxResults=10, singleEvents=True,
+                                              orderBy='startTime').execute()
+        events = events_result.get('items', [])
+
+        if not events:
+            print('No upcoming events found.')
+            return
+
+        # Prints the start and name of the next 10 events
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            print(start, event['summary'])
+
+service = authenticate_google()
+get_events(2, service)
